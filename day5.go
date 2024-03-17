5089589dad38a4fddcb023e11ace430def8215d3
Minier Anthony <aminier@splio.com>
2024-03-17 20:14:13 +0100

    fix result and now it takes only 2min

--- day5.go
+++ day5.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
-	"sync"
 )
 
 type data struct {
@@ -48,7 +46,7 @@ func day5() int {
 		value := seed
 		for _, m := range seedMap {
 			for _, d := range m {
-				if value >= d.source && value <= d.source+d.nrange {
+				if value >= d.source && value < d.source+d.nrange {
 					inc := d.destination - d.source
 					value = value + inc
 					break
@@ -68,7 +66,7 @@ type seed struct {
 	max int
 }
 
-// this solution (brute force one) take around 15 minutes to find the solution, not really optimized
+// this solution (brute force one) take around 2 minutes to find the solution, not really optimized
 // but i keep it and maybe later, i will try to find a better solution
 func day5_2() int {
 	minimum := 1000000000000
@@ -105,48 +103,24 @@ func day5_2() int {
 		nrange, _ := strconv.Atoi(parts[2])
 		seedMap[currentIndex] = append(seedMap[currentIndex], data{source: src, destination: dest, nrange: nrange})
 	})
-	buffer := make(chan struct{}, 10000)
-	finish := make(chan struct{})
-	result := make(chan int, 10000)
-	wg := sync.WaitGroup{}
-	totalToRun := 0
-	for _, s := range seeds {
-		totalToRun += s.max
-	}
-	wg.Add(totalToRun)
-	fmt.Println(totalToRun)
-	go func() {
-		for value := range result {
+
+	for _, seed := range seeds {
+		for seedValue := seed.min; seedValue < seed.min+seed.max; seedValue++ {
+			value := seedValue
+			for _, m := range seedMap {
+				for _, d := range m {
+					if value >= d.source && value < d.source+d.nrange {
+						inc := d.destination - d.source
+						value = value + inc
+						break
+					}
+				}
+			}
 			if value < minimum {
 				minimum = value
 			}
 		}
-		finish <- struct{}{}
-	}()
-	for _, seed := range seeds {
-		for seedValue := seed.min; seedValue < seed.min+seed.max; seedValue++ {
-			buffer <- struct{}{}
-			go func(seedValue int) {
-				value := seedValue
-				for _, m := range seedMap {
-					for _, d := range m {
-						if value >= d.source && value <= d.source+d.nrange {
-							inc := d.destination - d.source
-							value = value + inc
-							break
-						}
-					}
-				}
-
-				result <- value
-				<-buffer
-				wg.Done()
-			}(seedValue)
-		}
 	}
-	wg.Wait()
-	close(result)
-	<-finish
 
 	return minimum
 }
--- day5_test.go
+++ day5_test.go
@@ -11,9 +11,9 @@ func TestDay5(t *testing.T) {
 	assert.Equal(t, 111627841, result)
 }
 
-// this test timeout due to around 15 minutes to run this bruteforce method, there is probably
+// this test timeout due to around 2 minutes to run this bruteforce method, there is probably
 // a smarter solution but i don't know which one
 func TestDay5_2(t *testing.T) {
 	result := day5_2()
-	assert.Equal(t, 69323689, result)
+	assert.Equal(t, 69323688, result)
 }
